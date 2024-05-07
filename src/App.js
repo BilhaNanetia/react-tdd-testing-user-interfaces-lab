cc9625cf2a701b48a0c25f591363aae773c9bb75
Bilha <bilha.leposo@student.moringaschool.com>
2024-05-07 08:39:12 +0300

    completes user interfaces lab

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm Bilha</h1>
+      <img src="https://media.licdn.com/dms/image/D4D03AQHQds4It03iLQ/profile-displayphoto-shrink_800_800/0/1709565265243?e=1720656000&v=beta&t=nACavTkcIIK5TsTqkF9P8g6x8_8AcHiG32IA_L9F0lw" alt="Bilha Leposo" />
+      <h2>About Me</h2>
+      <p>Biography</p>
+      <a href="https://github.com/BilhaNanetia">GitHub</a>
+      <br/>
+      <a href="https://www.linkedin.com/in/bilha-leposo-1656392b8/">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
